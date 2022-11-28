5bd84cee77e996f8116b226ec368371ae2e158a7
alexandr.danilin <alexandr.danilin@ooma.com>
2022-11-28 09:30:36 +0300

    v2 golang version of plugin

--- internal/helpers/hhelper.go
+++ internal/helpers/hhelper.go
@@ -102,7 +102,7 @@ func resolveDeps(chart *models.MiniHelmChart, chartPath string, wp *workpool.Wor
 		_ = os.RemoveAll(cleanJoin(chartPath, "tmpcharts"))
 		_ = os.RemoveAll(cleanJoin(chartPath, "Chart.lock"))
 	}
-	if opts.SkipRefresh {
+	if opts.SkipRefresh || slices.Contains(opts.SkipRefreshInCharts, chart.Name) {
 		args = append(args, "--skip-refresh")
 	}
 	for _, dep := range chart.Dependencies {
@@ -163,7 +163,6 @@ func resolveDeps(chart *models.MiniHelmChart, chartPath string, wp *workpool.Wor
 }
 
 func ResolveDependencies(chartPath string, opts models.HelmResolveDepsOptions) error {
-	t := time.Now()
 	chart, err := LoadChartByPath(chartPath)
 	if err != nil {
 		return multierr.Append(err, fmt.Errorf("ensure that the chart directory (%v) exists", filepath.Dir(chartPath)))
@@ -172,9 +171,11 @@ func ResolveDependencies(chartPath string, opts models.HelmResolveDepsOptions) e
 	if err != nil {
 		return err
 	}
+	t := time.Now()
 	wp := workpool.New(opts.Threads)
-	// do not fetch any updates because we already might do it in updateRepositories
-	opts.SkipRefresh = true
+	if !opts.SkipRefresh {
+		opts.SkipRefresh = true
+	}
 	fmt.Printf("Resolving dependencies in %v chart ...\n", chart.Name)
 	err = resolveDeps(chart, chartPath, wp, opts)
 	if err != nil {
