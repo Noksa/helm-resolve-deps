2a3db9e8aabd4079b95beaa62b6c4ba032287276
alexandr.danilin <alexandr.danilin@ooma.com>
2022-11-28 10:01:44 +0300

    Revert "v2 golang version of plugin"
    This reverts commit f91e98649a31a65d52bfb23c2cb586e4ed5eff9c.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
 echo "Installing resolve-deps v${version} ..."
 
 # Find correct archive name
-unameOut="$(uname -s)"
+unameOut="$(uname -s | awk '{print tolower($0)}')"
 
 case "${unameOut}" in
     Linux*)     os=linux;;
