8ed916d189bd028adc86986cac47708106fdb89c
alexandr.danilin <alexandr.danilin@ooma.com>
2023-01-20 23:28:42 +0300

    Fixes #5

--- scripts/make_archieve.sh
+++ scripts/make_archieve.sh
@@ -4,11 +4,15 @@ mkdir -p ../generated
 cd ../generated
 version="$(cat ../plugin.yaml | grep "version" | cut -d '"' -f 2)"
 ARCH="amd64 arm64"
-OS="linux darwin"
+OS="linux darwin windows"
 for A in $ARCH; do
   for O in $OS; do
-    GOARCH=$A GOOS=$O go build -o "resolve-deps" ../cmd/resolve_deps.go
-    tar -czvf "resolve-deps_${version}_${O}_${A}.tar.gz" "resolve-deps"
-    rm -rf "resolve-deps"
+    output="resolve-deps"
+    if [[ "$O" == "windows" ]]; then
+      output="resolve-deps.exe"
+    fi
+    GOARCH=$A GOOS=$O go build -o "${output}" ../cmd/resolve_deps.go
+    tar -czvf "resolve-deps_${version}_${O}_${A}.tar.gz" "${output}"
+    #rm -rf "${output}"
   done
 done
